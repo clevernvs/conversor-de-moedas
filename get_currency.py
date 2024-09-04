e4f49d6d3c8288f0d93e75916d35d6374ec0f413
Cleverton Neves <clever.nvs@gmail.com>
2024-09-04 14:56:00 -0300

    feat: Incluindo o carregamento dos textos das moedas e dos conversores disponiveis

---
+++ get_currency.py
@@ -0,0 +1,26 @@
+import xmltodict
+
+def get_full_text():
+    with open("xml/currency.xml", "rb") as currency_archive:
+        dic_currency = xmltodict.parse(currency_archive)
+
+    currency = dic_currency["xml"]
+    return currency
+
+
+def get_all_available_currency_code_converters():
+    with open("xml/currency-converters.xml", "rb") as currency_converters_archive:
+        dic_currency_converters = xmltodict.parse(currency_converters_archive)
+
+    currency_converters = dic_currency_converters["xml"]
+
+    dic_converters_available = {}
+
+    for per_converter in currency_converters:
+        origin_currency_code, destiny_currency_code = per_converter.split("-")
+        if origin_currency_code in dic_converters_available:
+            dic_converters_available[origin_currency_code].append(destiny_currency_code)
+        else:
+            dic_converters_available[origin_currency_code] = destiny_currency_code
+
+    return dic_currency_converters
--- main.py
+++ main.py
@@ -1,50 +1,60 @@
 import customtkinter
+from get_currency import get_full_text, get_all_available_currency_code_converters
 
 # Definindo o tema de estilos da aplicação
 customtkinter.set_appearance_mode("dark")
 customtkinter.set_default_color_theme("dark-blue")
 
-# Definindo as configs da tela
+
+# Configurações da tela
 main_window = customtkinter.CTk()
 main_window.geometry("600x400")
 
-# Definindo os botões, textos e outros elementos
-main_window_label = customtkinter.CTkLabel(main_window, text="Conversor de moedas", font=("", 20))
+# Configurações os elementos
+header_label = customtkinter.CTkLabel(main_window, text="Conversor de moedas", font=("", 20))
 
-label_origin_coin = customtkinter.CTkLabel(main_window, text="Selecione a moeda de origem")
-label_destiny_coin = customtkinter.CTkLabel(main_window, text="Selecione a moeda de destino")
+label_origin_currency = customtkinter.CTkLabel(main_window, text="Selecione a moeda de origem")
+label_destiny_currency = customtkinter.CTkLabel(main_window, text="Selecione a moeda de destino")
 
-allTagsCoins = ["USD", "BRL", "EUR", "BTC"]
+dic_available_converters = get_all_available_currency_code_converters()
 
-input_origin_coin = customtkinter.CTkOptionMenu(main_window, values=allTagsCoins)
-input_destiny_coin = customtkinter.CTkOptionMenu(main_window, values=allTagsCoins)
+def get_destiny_currency(currency_code):
+    list_destiny_currency = dic_available_converters[currency_code]
+    input_destiny_currency.configure(values=list_destiny_currency)
+    input_destiny_currency.set(list_destiny_currency[0])
 
 
-def convert_coin():
+
+input_origin_currency = customtkinter.CTkOptionMenu(main_window, values=list(dic_available_converters.keys()), command=get_destiny_currency)
+input_destiny_currency = customtkinter.CTkOptionMenu(main_window, values="Selecione uma moeda de origem.")
+
+
+def convert_currency():
     print("Convertendo moeda...")
 
-button_convert_coin = customtkinter.CTkButton(main_window, text="Converter", command=convert_coin)
+button_convert_currency = customtkinter.CTkButton(main_window, text="Converter", command=convert_currency)
 
 
-scroll_area_list_coins = customtkinter.CTkScrollableFrame(main_window)
-allTextCoin = ["USD: Dólar Americano", "BRL: Real", "EUR: Euro", "BTC: Botcoin"]
-for coin in allTextCoin:
-    textCoin = customtkinter.CTkLabel(scroll_area_list_coins, text=coin)
-    textCoin.pack()
+scroll_area_list_currencys = customtkinter.CTkScrollableFrame(main_window)
+all_currency_full_text = get_full_text()
+for currency_code in all_currency_full_text:
+    currency_name = all_currency_full_text[currency_code]
+    textcurrency = customtkinter.CTkLabel(scroll_area_list_currencys, text=f"{currency_code}: {currency_name}" )
+    textcurrency.pack()
 
 
 # Definindo os elementos na tela
-main_window_label.pack(padx=10, pady=10)
+header_label.pack(padx=10, pady=10)
 
-label_origin_coin.pack(padx=10, pady=3)
-input_origin_coin.pack(padx=10, pady=10)
+label_origin_currency.pack(padx=10, pady=3)
+input_origin_currency.pack(padx=10, pady=10)
 
-label_destiny_coin.pack(padx=10, pady=3)
-input_destiny_coin.pack(padx=10, pady=10)
+label_destiny_currency.pack(padx=10, pady=3)
+input_destiny_currency.pack(padx=10, pady=10)
 
-button_convert_coin.pack(padx=10, pady=10)
+button_convert_currency.pack(padx=10, pady=10)
 
-scroll_area_list_coins.pack(padx=10, pady=10)
+scroll_area_list_currencys.pack(padx=10, pady=10)
 
 
 # Exibindo a tela
